9ba8dac5af60076b26598f3ae32dcec94be93a8c
Alberto Cortes <cortesgarcia2010@gmail.com>
2021-03-04 20:28:46 -0600

    Initial code

---
+++ index.js
@@ -0,0 +1,32 @@
+const { Portal } = require('@material-ui/core');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Ocupar /Sopas para el catálogo');
+});
+
+app.get('/sopas', (req, res) => {
+    const sopas = [
+        {
+            producto: 'Sopa de Res',
+            precio: 100,
+            descripcion: 'Acompañada con arroz y tortilla'
+        },
+        {
+            producto: 'Sopa de Frijoles',
+            precio: 120,
+            descripcion: 'Acompañada con arroz, tortilla y chicharrones'
+        },
+    ];
+
+    res.json(sopas);
+});
+
+const port = 3003;
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
